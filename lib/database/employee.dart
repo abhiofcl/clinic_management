df90899979c6af612bcff7f9c6a3be9a3d95997f
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-12-29 21:26:38 +0530

    added employee class for database

---
+++ lib/database/employee.dart
@@ -0,0 +1,81 @@
+// patient.g.dart - Generate this file using: flutter pub run build_runner build
+import 'dart:typed_data';
+
+import 'package:hive/hive.dart';
+
+part 'employee.g.dart';
+
+@HiveType(typeId: 0)
+class Employee extends HiveObject {
+  @HiveField(0)
+  String name;
+
+  @HiveField(1)
+  int age;
+
+  @HiveField(2)
+  String gender;
+
+  @HiveField(3)
+  String address;
+
+  @HiveField(4)
+  String phoneNumber;
+
+  @HiveField(5)
+  String idProofType;
+
+  @HiveField(6)
+  String idProof;
+
+  @HiveField(7)
+  Uint8List? image;
+
+  @HiveField(8)
+  List<DutySheetEntry>? dutySheet;
+
+  @HiveField(9)
+  String? status;
+
+  Employee({
+    required this.name,
+    required this.age,
+    required this.address,
+    required this.gender,
+    required this.phoneNumber,
+    required this.idProofType,
+    required this.idProof,
+    this.image,
+    this.dutySheet,
+    this.status,
+  });
+}
+
+@HiveType(typeId: 1)
+class DutySheetEntry extends HiveObject {
+  @HiveField(0)
+  DateTime? date;
+
+  @HiveField(1)
+  String? timeSlot;
+
+  @HiveField(2)
+  String? patientName;
+
+  @HiveField(3)
+  String? qty;
+
+  @HiveField(4)
+  String? rate;
+
+  @HiveField(5)
+  String? amount;
+
+  DutySheetEntry({
+    this.date,
+    this.timeSlot,
+    this.qty,
+    this.rate,
+    this.amount,
+  });
+}
